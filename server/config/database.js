ca5cdab151a0c2d0f4aec72098695e31d4aaee4e
nicoledl <nicolelosana@gmail.com>
2023-09-11 12:40:05 -0300

    The database config was modified.

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-  username: process.env.DB_USERNAME || 3000,
+  username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "root2305",
   database: process.env.DB_DATABASE || "sequelize",
-  host: process.env.DB_HOST || localhost,
-  dialect: process.env.DB_DIALECT || mysql, 
+  host: process.env.DB_HOST || "localhost",
+  dialect: process.env.DB_DIALECT || "mysql", 
   define: {
     timestamps: false,
   },
