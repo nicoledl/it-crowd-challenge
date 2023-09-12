67c90e6705191ad58fd964761c24b634f40cacd8
nicoledl <nicolelosana@gmail.com>
2023-09-12 17:12:19 -0300

    Modal edited

--- client/src/components/store/Modal.jsx
+++ client/src/components/store/Modal.jsx
@@ -17,8 +17,8 @@ const Modal = ({ closeMondal, product }) => {
 
   return (
     <div className="modal w-screen h-screen top-0 left-0 fixed z-20 bg-black/10 backdrop-blur">
-      <div className="animate-jump-in animate-once animate-delay-[200ms] animate-normal drop-shadow-lg absolute top-0 bottom-0 left-0 right-0 mx-5 sm:mx-auto sm:w-[300px] md:w-[600px] h-fit sm:h-[500px] md:h-[400px] dark:bg-neutral-800 dark:text-white bg-white pt-8 p-2 md:pt-3 md:p-3">
-        <div className="modal-content md:flex h-fit pb-5">
+      <div className="animate-jump-in animate-once animate-delay-[200ms] animate-normal drop-shadow-lg absolute m-auto top-0 bottom-0 left-0 right-0 w-[300px] md:w-[600px] h-[500px] md:h-[400px] dark:bg-neutral-800 dark:text-white bg-white pt-8 p-2 md:pt-3 md:p-3">
+        <div className="modal-content md:flex h-full">
           <img
             className="image-modal object-cover md:w-[180px] w-full h-[50%] md:h-full hover:h-full md:hover:w-full ease-out duration-300 "
             src={
