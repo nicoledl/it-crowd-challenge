183f108043e19b8b63b120d060b8094472296c21
nicoledl <nicolelosana@gmail.com>
2023-09-12 16:55:02 -0300

    Brands page edited

--- client/src/components/store/Brands.jsx
+++ client/src/components/store/Brands.jsx
@@ -34,35 +34,43 @@ const Brands = () => {
     <>
       <section className="container mx-auto px-8 sm:px-4 xl:px-44 text-black/80">
         <div className="grid justify-center grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 sm:gap-4">
-          {brands.map((brand, i) => (
-            <div key={i} className="p-10 flex justify-center align-center">
-              {brand.image_url ? (
-                <img
-                  src={
-                    brand.image_url
-                      ? brand.image_url
-                      : "https://www.pulsorunner.com/wp-content/uploads/2014/10/default-img.gif"
-                  }
-                  alt={brand.name}
-                  onClick={() => onClick(brand.name)}
-                  className="rounded object-contain cursor-pointer dark:invert hover:animate-pulse animate-once"
-                />
-              ) : (
-                <p
-                  className="text-3xl text-bolder cursor-pointer dark:invert"
-                  onClick={() => onClick(brand.name)}
-                >
-                  {brand.name}
-                </p>
-              )}
-            </div>
-          ))}
+          {brands.length !== 0 ? (
+            <>
+              {brands.map((brand, i) => (
+                <div key={i} className="p-10 flex justify-center align-center">
+                  {brand.image_url ? (
+                    <img
+                      src={
+                        brand.image_url
+                          ? brand.image_url
+                          : "https://www.pulsorunner.com/wp-content/uploads/2014/10/default-img.gif"
+                      }
+                      alt={brand.name}
+                      onClick={() => onClick(brand.name)}
+                      className="rounded object-contain cursor-pointer dark:invert hover:animate-pulse animate-once"
+                    />
+                  ) : (
+                    <p
+                      className="text-3xl text-bolder cursor-pointer dark:invert"
+                      onClick={() => onClick(brand.name)}
+                    >
+                      {brand.name}
+                    </p>
+                  )}
+                </div>
+              ))}
+              <Pagination
+                setPage={setPage}
+                url={`${process.env.NEXT_PUBLIC_URL_SERVER}/api/brands/`}
+              />
+            </>
+          ) : (
+            <p className="animate-bounce animate-infinite text-center mt-44 text-3xl">
+              There's not brands yet... :(
+            </p>
+          )}
         </div>
       </section>
-      <Pagination
-        setPage={setPage}
-        url={`${process.env.NEXT_PUBLIC_URL_SERVER}/api/brands/`}
-      />
     </>
   );
 };
