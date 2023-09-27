1131aa905cbb74ae6c404e2fb8829cf488265826
nicoledl <nicolelosana@gmail.com>
2023-09-27 16:19:15 -0300

    Message added

--- client/src/components/store/Brands.jsx
+++ client/src/components/store/Brands.jsx
@@ -25,6 +25,7 @@ const Brands = () => {
         className="absolute top-0 bottom-0 left-0 right-0 m-auto h-screen justify-center flex items-center"
         style={{ alignItems: "center", zIndex: "-2" }}
       >
+        <p>This may take a couple of minutes.</p>
         <BeatLoader color="#F43F5E" size={25} />
       </div>
     );
@@ -66,12 +67,12 @@ const Brands = () => {
             </p>
           )}
         </div>
-          {brands.length !== 0 && (
-            <Pagination
-              setPage={setPage}
-              url={`${process.env.NEXT_PUBLIC_URL_SERVER}/api/brands/`}
-            />
-          )}
+        {brands.length !== 0 && (
+          <Pagination
+            setPage={setPage}
+            url={`${process.env.NEXT_PUBLIC_URL_SERVER}/api/brands/`}
+          />
+        )}
       </section>
     </>
   );
