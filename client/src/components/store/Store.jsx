073b81390934d69a538a9e5ea89724d23c82a39a
nicoledl <nicolelosana@gmail.com>
2023-09-27 16:51:05 -0300

    BeatLoader fixed

--- client/src/components/store/Store.jsx
+++ client/src/components/store/Store.jsx
@@ -26,8 +26,8 @@ const Store = () => {
         className="absolute top-0 bottom-0 left-0 right-0 m-auto h-screen justify-center flex items-center"
         style={{ alignItems: "center", zIndex: "-2" }}
       >
-        <div className="grid">
-          <p>This may take a couple of minutes.</p>
+        <p>This may take a couple of minutes.</p>
+        <div>
           <BeatLoader color="#F43F5E" size={25} />
         </div>
       </div>
