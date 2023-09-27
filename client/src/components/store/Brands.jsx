8d21fd0b4569ac2fe3dae3441887d8c948918d5a
nicoledl <nicolelosana@gmail.com>
2023-09-27 16:53:19 -0300

    BeatLoader fixed

--- client/src/components/store/Brands.jsx
+++ client/src/components/store/Brands.jsx
@@ -22,12 +22,14 @@ const Brands = () => {
   if (!brands) {
     return (
       <div
-        className="absolute grid top-0 bottom-0 left-0 right-0 m-auto h-screen justify-center flex items-center"
+        className="absolute top-0 bottom-0 left-0 right-0 m-auto h-screen justify-center flex items-center"
         style={{ alignItems: "center", zIndex: "-2" }}
       >
-        <p>This may take a couple of minutes.</p>
-        <div>
-          <BeatLoader color="#F43F5E" size={25} />
+        <div className="grid">
+          <p>This may take a couple of minutes.</p>
+          <div>
+            <BeatLoader color="#F43F5E" size={25} />
+          </div>
         </div>
       </div>
     );
